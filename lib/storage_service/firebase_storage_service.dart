1efe32fe2f8eecf469a9b46f046c996cbadef6eb
billabrian6 <billabrian6@gmail.com>
2021-11-23 23:54:16 +0000

    added files for storage and login services

---
+++ lib/storage_service/firebase_storage_service.dart
@@ -0,0 +1,10 @@
+import 'package:new_world_watermark/storage_service/storage_service.dart';
+
+class FirebaseStorageService extends StorageService {
+  @override
+  Future<int> getWatermarkForItem(int itemID) {
+    // TODO: implement getWatermarkForItem
+    throw UnimplementedError();
+  }
+  
+}
---
+++ lib/storage_service/storage_service.dart
@@ -0,0 +1,3 @@
+abstract class StorageService {
+  Future<int> getWatermarkForItem(int itemID);
+}
